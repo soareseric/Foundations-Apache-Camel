7d3ab03e45d10a4d15cbda13b148a2ef1b91e6be
soareseric <contato.eric.sm@gmail.com>
2018-12-03 10:59:19 -0200

    Usando a exchange HttpMethod e HttpQuery

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,7 +1,9 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -21,10 +23,12 @@ public class RotaPedidos {
 					filter().
 						xpath("/item/formato[text()='EBOOK']").
 					log("${id}").
-					marshal().xmljson().
+					marshal().
+						xmljson().
 					log("${body}").
-					setHeader("CamelFileName", simple("${file:name.noext}.json")).
-				to("file:saida");
+					setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+					setHeader(Exchange.HTTP_QUERY, constant("ebookId=ARQ&pedidoId=2451256&clienteId=edgar.b@abc.com")).
+				to("http4://localhost:8080/webservices/ebook/item");
 			}
 		});
 		
